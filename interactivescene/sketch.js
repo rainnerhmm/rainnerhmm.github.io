1c3484deb7a748288a38b433fe72ca8c968bdcc9
rainner <rainner_hmm@outlook.com>
2024-09-19 14:14:35 -0600

    p5js added

---
+++ interactivescene/sketch.js
@@ -0,0 +1,16 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+let  bgColor = [5, 0, 32];
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(bgColor)
+}
