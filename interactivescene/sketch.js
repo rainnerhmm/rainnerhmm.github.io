2a5adabb4357b6a6c66c2c06675f5411962f3239
rainner <rainner_hmm@outlook.com>
2024-09-23 14:15:55 -0600

    added icon and 'papers' and 'scissors' option

--- interactivescene/sketch.js
+++ interactivescene/sketch.js
@@ -5,6 +5,8 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 key1 = 49
+key2 = 50
+key3 = 51
 choices = ["rock", "paper","scissors"];
 let  bgColor = [5, 0, 32];
 function setup() {
@@ -17,9 +19,17 @@ function draw() {
 }
 
 function rockVar() {
-  if (keyIsDown(key1));{
+  if (keyIsDown(key1)) {
     fill(255)
-    text("rock", windowWidth/2, windowHeight/2)
+    text("rocks", windowWidth/2, windowHeight/2)
+  }
+  else if (keyIsDown(key2)) {
+    fill(255)
+    text("papers", windowWidth/2, windowHeight/2)
+  }
+  else if (keyIsDown(key3)) {
+    fill(255)
+    text("scissors", windowWidth/2, windowHeight/2)
   }
 }
 
