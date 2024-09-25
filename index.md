bb3e23d05783000ab70cc228fbdafb9e467e765d
rainner <rainner_hmm@outlook.com>
2024-09-25 13:27:38 -0600

    Revert "broken commit?"
    This reverts commit 68cea62a355418639ca22a19a314b0fb8024c283.

--- index.md
+++ index.md
@@ -3,25 +3,6 @@
 ## demos
 - [movin' square](movingsquare)
 - [imaging](imagedemo)
+- [trafficlighting](trafficlighting)
 ## projects
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-- [rocks! papers!! scissors!!! | interactive project](interactivescene)
-=======
 - [interactive project](interactivescene)
->>>>>>> parent of 0cd6229 (Updated interactive project title)
-=======
-- [interactive project](interactivescene)
->>>>>>> parent of 0cd6229 (Updated interactive project title)
-=======
-- [interactive project](interactivescene)
->>>>>>> parent of 0cd6229 (Updated interactive project title)
-=======
-- [interactive project](interactivescene)
->>>>>>> parent of 0cd6229 (Updated interactive project title)
-=======
-- [interactive project](interactivescene)
->>>>>>> parent of 0cd6229 (Updated interactive project title)
---
+++ trafficlighting/sketch.js
@@ -0,0 +1,65 @@
+// Traffic Lighting
+// Rainn Morphy
+// September 24th, 2024
+
+// GOAL: make a 'traffic light' simulator. For now, just have the light
+// changing according to time. You may want to investigate the millis()
+
+let trafficState = ["green", "yellow", "red"]
+let lastSwitchedTime = 0;
+let waitTime = 2000;
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(255);
+  drawOutlineOfLights();
+  swapState();
+  trafficLights();
+}
+
+function drawOutlineOfLights() {
+  //box
+  rectMode(CENTER);
+  fill(0);
+  rect(width/2, height/2, 75, 200, 10);
+
+  //lights
+  fill(255);
+  ellipse(width/2, height/2 - 65, 50, 50); //top
+
+  ellipse(width/2, height/2, 50, 50); //middle
+
+  ellipse(width/2, height/2 + 65, 50, 50); //bottom
+}
+
+function trafficLights(){
+  if (trafficState === "red"){
+    //top
+    fill(220, 0, 115);
+    ellipse(width/2, height/2 - 65, 50, 50); 
+  }
+  else if (trafficState === "yellow"){
+    //middle
+    fill(234, 196, 53);
+    ellipse(width/2, height/2, 50, 50);
+  }
+  else if (trafficState === "green"){
+    //bottom
+    fill(4, 231, 98);
+    ellipse(width/2, height/2 + 65, 50, 50);
+  }
+}
+
+function swapState(){    
+  trafficState = trafficState[0];
+  if (millis() > lastSwitchedTime + waitTime) {
+
+    lastSwitchedTime = millis();
+  }
+}
+
+
+
