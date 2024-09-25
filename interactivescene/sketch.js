87b85dbc83fbfa96bee09824c1e274419f03512b
rainner <rainner_hmm@outlook.com>
2024-09-25 14:16:02 -0600

    added opponent to rock paper scissors

--- interactivescene/sketch.js
+++ interactivescene/sketch.js
@@ -1,37 +1,55 @@
-// Project Title
-// Your Name
-// Date
+// Interactive Scene
+// Rainn Morphy
+// September 25th, 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 let key1 = 49;
 let key2 = 50;
 let key3 = 51;
-let choices = ["rock", "paper","scissors"];
-let  bgColor = [5, 0, 32];
+let choices = ["rocks", "papers","scissors"];
+let bgColor = [5, 0, 32];
+let playerTurn = true;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(bgColor);
-  rockVar();
+  temp();
+  keyTyped()
 }
 
-function rockVar() {
-  if (keyIsDown(key1)) {
+function keyTyped(){
+  if (key === key3){
     fill(255);
-    text("rocks", windowWidth/2, windowHeight/2);
-  }
-  else if (keyIsDown(key2)) {
-    fill(255);
-    text("papers", windowWidth/2, windowHeight/2);
-  }
-  else if (keyIsDown(key3)) {
-    fill(255);
-    text("scissors", windowWidth/2, windowHeight/2);
+    text(choices[2], windowWidth/2, windowHeight/2);
+    playerTurn = false
   }
 }
-millis();
 
+function temp() {
+  if (playerTurn === true) {
+    if (keyIsDown(key1)) {
+      fill(255);
+      text(choices[0], windowWidth/2, windowHeight/2);
+    }
+    else if (keyIsDown(key2)) {
+      fill(255);
+      text(choices[1], windowWidth/2, windowHeight/2);
+    }
+    else if (keyIsDown(key3)) {
+      fill(255);
+      text(choices[2], windowWidth/2, windowHeight/2);
+    }
+  }
+}
+
+function temp2(){
+  if (playerTurn === false) {
+    let oppselection = random(0,2);
+    text(choices[Math.round(oppselection)], windowWidth/2, windowHeight/2);
+    playerTurn = true;
+  }
+}
 
