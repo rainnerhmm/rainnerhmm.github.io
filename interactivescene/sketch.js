e7ff6d3995e8fa424f00cc41a94c7caf4316a68a
rainner <rainner_hmm@outlook.com>
2024-09-27 14:16:56 -0600

    refined code, fixed text back to where I started an hour ago, but better I guess

--- interactivescene/sketch.js
+++ interactivescene/sketch.js
@@ -4,49 +4,69 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let key1 = 49;
-let key2 = 50;
-let key3 = 51;
-let choices = ["rocks", "papers","scissors"];
+
+// 1's keycode is 49
+// 2's keycode is 50
+// 3's keycode is 51
+let choices = ["rocks", "papers", "scissors"];
 let bgColor = [5, 0, 32];
-let playerTurn = true;
+let textOnScreen = false;
+let rocks = false;
+let papers = false;
+let scissors = false;
+let optionChosen = false;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(bgColor);
-  keyTyped();
-  oppenentTurn
+  rpsMove();
+  // oppenentTurn;
 }
 
-function keyTyped(){
-  if (keyIsDown(key1)) {
+function rpsMove() {
+  if (rocks === true) {
     fill(255);
     textAlign(LEFT);
     text(choices[0], width / 4, height / 2);
   }
-  else if (keyIsDown(key2)) {
-    fill(255);
-    textAlign(LEFT);
-    text(choices[1], width / 4, height / 2);
+}
+
+//   fill(255);
+//   textAlign(LEFT);
+//   text(choices[1], width / 4, height / 2);
+
+
+//   fill(255);
+//   textAlign(LEFT);
+//   text(choices[2], width / 4, height / 2);
+// }
+
+function keyTyped() {
+  if (keyCode === 49) {
+    rocks = true;
   }
-  else if (keyIsDown(key3)) {
-    fill(255);
-    textAlign(LEFT);
-    text(choices[2], width / 4, height / 2);
+  else if (keyCode === 50) {
+    papers = true;
+  }
+  else if (keyCode === 51) {
+    scissors = true;
   }
 }
 
+// function oppenentTurn(){
+//   if (playerTurn === false) {
+//     let oppselection = random(0,2);
+//     text(choices[Math.round(oppselection)], width / -4, height/2);
+//     playerTurn = true;
+//   }
+// }
 
-function oppenentTurn(){
-  if (playerTurn === false) {
-    let oppselection = random(0,2);
-    text(choices[Math.round(oppselection)], width / -4, height/2);
-    playerTurn = true;
-  }
-}
-
-function windowResized(){
+function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function rpsLogic() {
 }
