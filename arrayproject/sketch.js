d6662276f03d6333a6e510e8f6750279947654b7
rainner <rainner_hmm@outlook.com>
2024-10-15 21:31:42 -0600

    continued work on bobber and its interaction with collision

--- arrayproject/sketch.js
+++ arrayproject/sketch.js
@@ -5,6 +5,8 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let gravity = 0;
+let bobberY = 50;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,9 +15,19 @@ function setup() {
 function draw() {
   background(220);
   bobber();
+  ground();
 }
 
 function bobber() {
-  fill(255,0,0)
-  circle(50, 50, 25)
+  fill(255,0,0);
+  circle(50, bobberY, 25);
+  if (bobberY < height-250){
+    gravity++;
+    bobberY = bobberY + gravity * 2;
+  } 
+}
+
+function ground() {
+  fill(0, 255, 0);
+  rect(0, height-200, width, 200)
 }
