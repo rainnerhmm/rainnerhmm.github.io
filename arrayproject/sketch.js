a25c620965d86e081e58ccb6922680947503e735
rainner <rainner_hmm@outlook.com>
2024-10-18 22:16:14 -0600

    began redoing fishline logic

--- arrayproject/sketch.js
+++ arrayproject/sketch.js
@@ -5,18 +5,17 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// inspired by https://editor.p5js.org/gabriel.lee/sketches/a1BR-maEV
+
 // hook physics
 let hook = {
   x: 50, // hook x coords
   y: 50, // hook y coords
   d: 25, // diameter of hook
-  weight: 0, // 
-  resistence: 0, 
+  weight: 0, // weight of hook
+  resistence: 0, // air resistence
   velocity: 5, // velocity of hook
 };
-
-// missing variables (inspired by https://editor.p5js.org/gabriel.lee/sketches/a1BR-maEV)
-
 // resistence/damper; makes the hook not a perpetual motion machine // velocity should be affected by resistance
 // weight; the weight of the "hook" should play into it's resistence
 
@@ -27,8 +26,12 @@ let hook = {
 // if fish attaches > increases weight > increases resistence > slows velocity
 
 // rod/fishing line logic
-let rodHookDist; // distance from rod (mouse) to hook
-let strain = 300; // maximum distance rod can extend // strain should have a threshold rather than a sole number, strain should be able...
+let fishLine = {
+  dist: 0, // distance from rod (mouse) to hook
+  maxLength: 300, // the maximum distance the line can extend without straining
+
+};
+// maximum distance rod can extend // strain should have a threshold rather than a sole number, strain should be able...
 // ...to extend farther than maximum before snapping
 
 // if the fish pulls it cause the fishing line to fully extend, putting pressure on the line...
@@ -51,28 +54,36 @@ function setup() {
 function draw() {
   background(220);
 
-  fishLine();
+  fishLine2();
   hook2();
 
-  // console.log(rodHookDist);
-  console.log(hook.velocity);
+  // console.log(fishLine.dist);
+  textSize(48);
+  text(fishLine.dist, width/2, height/2);
 }
 
+// 
+
 function hook2() {
-  // fishLine(hook.x, hook.y, mouseX, mouseY);
-  // rodHookDist = dist(hook.x, hook.y, mouseX, mouseY);
   fill(150);
   circle(hook.x, hook.y, hook.d);
-  if (rodHookDist <= strain){ // going down
-    hook.velocity++;
-    strain = strain + 0.1;
-    hook.y = hook.y + hook.velocity;
-  }
-  else if (rodHookDist >= strain){ // going up
+  hook.y = hook.y + hook.velocity;
+  hook.velocity++;
+  if (fishLine.maxLength <= fishLine.dist){
     hook.velocity--;
-    strain = strain - 0.1;
-    hook.y = hook.y + hook.velocity;
+    hook.y = hook.y - hook.velocity;
   }
+  // if (fishLine.dist <= fishLine.maxLength){ // going down
+  //   hook.velocity++;
+  //   fishLine.maxLength = fishLine.maxLength + 0.1;
+  //   hook.y = hook.y + hook.velocity;
+  // }
+  // else if (fishLine.dist >= fishLine.maxLength){ // going up
+  //   hook.velocity--;
+  //   fishLine.maxLength = fishLine.maxLength - 0.1;
+  //   hook.y = hook.y + hook.velocity;
+  // }
+
 }
 function mouseClicked(){
   hook.y = mouseY;
@@ -83,14 +94,20 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function fishLine (){
-  stroke(rodHookDist, 0, 0);
+function fishLine2 (){
+  fishLineTension();
   line(hook.x, hook.y, mouseX, mouseY);
-  rodHookDist = dist(hook.x, hook.y, mouseX, mouseY);
+  fishLine.dist = dist(hook.x, hook.y, mouseX, mouseY);
 }
 
 function fishLineTension(){
 
+  if (fishLine.dist >= fishLine.maxLength) {
+    stroke(fishLine.dist-fishLine.maxLength, 0, 0);
+  }
+  else {
+    stroke(0, 0, 0);
+  }
 }
 
 
