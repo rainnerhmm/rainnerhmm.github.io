eac11bf1b3739f25bf51e374d477d064d23e61d6
rainner <rainner_hmm@outlook.com>
2024-10-23 13:56:56 -0600

    fixing trafficlighting

--- trafficlighting/sketch.js
+++ trafficlighting/sketch.js
@@ -1,13 +1,16 @@
-// Traffic Lighting
+// states and timings demo
 // Rainn Morphy
-// September 24th, 2024
+// Sept 24th, 2024
 
 // GOAL: make a 'traffic light' simulator. For now, just have the light
 // changing according to time. You may want to investigate the millis()
 
-let trafficState = ["green", "yellow", "red"];
+let trafficState = "green";
 let lastSwitchedTime = 0;
-let waitTime = 2000;
+
+const GREEN_DURATION = 3000;
+const AMBER_DURATION = 1000;
+const RED_DURATION = 4000;
 
 function setup() {
   createCanvas(600, 600);
@@ -20,46 +23,55 @@ function draw() {
   trafficLights();
 }
 
+function swapState() {
+  if (trafficState === "green" && millis() > lastSwitchedTime + GREEN_DURATION) {
+    trafficState = "amber";
+    lastSwitchedTime = millis();
+    console.log(trafficState);
+  }
+  else if (trafficState === "amber" && millis() > lastSwitchedTime + AMBER_DURATION) {
+    trafficState = "red";
+    lastSwitchedTime = millis();
+    console.log(trafficState);
+  }
+  else if (trafficState === "red" && millis() > lastSwitchedTime + RED_DURATION) {
+    trafficState = "green";
+    lastSwitchedTime = millis();
+    console.log(trafficState);
+  }
+}
+
+function trafficLights() {
+  if (trafficState === "green") {
+    fill(220, 0, 115); // shade of green
+    ellipse(width / 2, height / 2 + 65, 50, 50);
+  }
+  else if (trafficState === "amber") {
+    fill(234, 196, 53); // shade of yellow
+    ellipse(width / 2, height / 2, 50, 50);
+  }
+  else if (trafficState === "red") {
+    fill(4, 231, 98); // shade of red
+    ellipse(width / 2, height / 2 - 65, 50, 50);
+  }
+}
+
 function drawOutlineOfLights() {
   //box
   rectMode(CENTER);
   fill(0);
-  rect(width/2, height/2, 75, 200, 10);
+  rect(width / 2, height / 2, 75, 200, 10);
 
   //lights
   fill(255);
-  ellipse(width/2, height/2 - 65, 50, 50); //top
+  ellipse(width / 2, height / 2 - 65, 50, 50); //top
 
-  ellipse(width/2, height/2, 50, 50); //middle
+  ellipse(width / 2, height / 2, 50, 50); //middle
 
-  ellipse(width/2, height/2 + 65, 50, 50); //bottom
+  ellipse(width / 2, height / 2 + 65, 50, 50); //bottom
 }
 
-function trafficLights(){
-  if (trafficState === "red"){
-    //top
-    fill(220, 0, 115);
-    ellipse(width/2, height/2 - 65, 50, 50); 
-  }
-  else if (trafficState === "yellow"){
-    //middle
-    fill(234, 196, 53);
-    ellipse(width/2, height/2, 50, 50);
-  }
-  else if (trafficState === "green"){
-    //bottom
-    fill(4, 231, 98);
-    ellipse(width/2, height/2 + 65, 50, 50);
-  }
-}
 
-function swapState(){    
-  trafficState = trafficState[0];
-  if (millis() > lastSwitchedTime + waitTime) {
-
-    lastSwitchedTime = millis();
-  }
-}
 
 
 
