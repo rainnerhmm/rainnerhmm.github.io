9e75a46d00747e2f01a7e6fb8ce4c67be305619e
rainner <rainner_hmm@outlook.com>
2024-10-23 13:57:39 -0600

    finished magicalgrid demo

--- magicalgrids/sketch.js
+++ magicalgrids/sketch.js
@@ -12,8 +12,9 @@
 // ];
 
 let magicalGrid;
-const MAGICAL_GRID_SIZE = 4;
+const MAGICAL_GRID_SIZE = 16;
 let magicalCellSize;
+let magicalNeighbourToggler = true;
 
 function setup() {
   if (windowWidth < windowHeight) {
@@ -26,9 +27,47 @@ function setup() {
   magicalGrid = summonRandomMagicalGrid(MAGICAL_GRID_SIZE, MAGICAL_GRID_SIZE);
 }
 
+function windowResized() {
+  if (windowWidth < windowHeight) {
+    resizeCanvas(windowWidth, windowWidth);
+  }
+  else {
+    resizeCanvas(windowHeight, windowHeight);
+  }
+  magicalCellSize = height / MAGICAL_GRID_SIZE;
+}
+
 function draw() {
   background(220);
   magicalGridDisplayer();
+}
+
+function mousePressed() {
+  let x = Math.floor(mouseX / magicalCellSize);
+  let y = Math.floor(mouseY / magicalCellSize);
+
+  // toggle self
+  magicalCellToggle(x, y);
+
+  // toggle neighbours
+  if (magicalNeighbourToggler) {
+    magicalCellToggle(x + 1, y);
+    magicalCellToggle(x - 1, y);
+    magicalCellToggle(x, y + 1);
+    magicalCellToggle(x, y - 1);
+  }
+}
+
+function magicalCellToggle(x, y) {
+  // make sure the cell you're toggling is in the grid
+  if (x >= 0 && y >= 0 && x < MAGICAL_GRID_SIZE && y < MAGICAL_GRID_SIZE) {
+    if (magicalGrid[y][x] === 1) {
+      magicalGrid[y][x] = 0;
+    }
+    else {
+      magicalGrid[y][x] = 1;
+    }
+  }
 }
 
 function keyPressed() {
@@ -37,6 +76,9 @@ function keyPressed() {
   }
   if (key === "e") {
     magicalGrid = summonEmptyMagicalGrid(MAGICAL_GRID_SIZE, MAGICAL_GRID_SIZE);
+  }
+  if (key === "n") {
+    magicalNeighbourToggler = !magicalNeighbourToggler;
   }
 }
 
