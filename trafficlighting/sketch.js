593f6b3793751b08e1b9b948b181d68a3e1f501d
rainner <rainner_hmm@outlook.com>
2024-10-24 19:03:32 -0600

    trafficlighting can now reliably control the flow of traffic
    somewhat?

--- trafficlighting/sketch.js
+++ trafficlighting/sketch.js
@@ -5,12 +5,12 @@
 // GOAL: make a 'traffic light' simulator. For now, just have the light
 // changing according to time. You may want to investigate the millis()
 
-let trafficState = "green";
-let lastSwitchedTime = 0;
+let trafficState = "green"; // state of activated traffic light
+let lastSwitchedTime = 0; // keeps trafficlights on time
 
-const GREEN_DURATION = 3000;
-const AMBER_DURATION = 1000;
-const RED_DURATION = 4000;
+const GREEN_DURATION = 3000; // greenlight is activated for 3 seconds
+const AMBER_DURATION = 1000; // amberlight is activated for 3 seconds
+const RED_DURATION = 4000; // redlight is activated for 3 seconds
 
 function setup() {
   createCanvas(600, 600);
@@ -18,57 +18,58 @@ function setup() {
 
 function draw() {
   background(255);
-  drawOutlineOfLights();
-  swapState();
-  trafficLights();
+
+  trafficBox(); // displays the trafficlight box
+  swapTrafficState(); // swaps which trafficlight is activated
+  trafficLights(); // displays activated trafficlight
 }
 
-function swapState() {
+function swapTrafficState() {
   if (trafficState === "green" && millis() > lastSwitchedTime + GREEN_DURATION) {
     trafficState = "amber";
-    lastSwitchedTime = millis();
+    lastSwitchedTime = millis(); // resets timer from current millisecond
     console.log(trafficState);
   }
   else if (trafficState === "amber" && millis() > lastSwitchedTime + AMBER_DURATION) {
     trafficState = "red";
-    lastSwitchedTime = millis();
+    lastSwitchedTime = millis(); // resets timer from current millisecond
     console.log(trafficState);
   }
   else if (trafficState === "red" && millis() > lastSwitchedTime + RED_DURATION) {
     trafficState = "green";
-    lastSwitchedTime = millis();
+    lastSwitchedTime = millis(); // resets timer from current millisecond
     console.log(trafficState);
   }
 }
 
 function trafficLights() {
   if (trafficState === "green") {
-    fill(220, 0, 115); // shade of green
-    ellipse(width / 2, height / 2 + 65, 50, 50);
+    fill(4, 231, 98); // shade of green
+    ellipse(width / 2, height / 2 + 65, 50, 50); // bottom/green
   }
   else if (trafficState === "amber") {
     fill(234, 196, 53); // shade of yellow
-    ellipse(width / 2, height / 2, 50, 50);
+    ellipse(width / 2, height / 2, 50, 50); // middle/amber
   }
   else if (trafficState === "red") {
-    fill(4, 231, 98); // shade of red
-    ellipse(width / 2, height / 2 - 65, 50, 50);
+    fill(220, 0, 115); // shade of red
+    ellipse(width / 2, height / 2 - 65, 50, 50); // top/red
   }
 }
 
-function drawOutlineOfLights() {
-  //box
+function trafficBox() {
+  // trafficbox
   rectMode(CENTER);
   fill(0);
-  rect(width / 2, height / 2, 75, 200, 10);
+  rect(width / 2, height / 2, 75, 200, 10); // shade of black
 
-  //lights
+  // trafficlights
   fill(255);
-  ellipse(width / 2, height / 2 + 65, 50, 50); // top
+  ellipse(width / 2, height / 2 - 65, 50, 50); // top
 
   ellipse(width / 2, height / 2, 50, 50); // middle
 
-  ellipse(width / 2, height / 2 - 65, 50, 50); // bottom
+  ellipse(width / 2, height / 2 + 65, 50, 50); // bottom
 }
 
 
