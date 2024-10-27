a63ebf1a8f74451052ec5f6c92ba433554e762a7
rainner <rainner_hmm@outlook.com>
2024-10-26 22:32:39 -0600

    added project description

--- p03-gridbased/sketch.js
+++ p03-gridbased/sketch.js
@@ -1,9 +1,18 @@
-// Project Title
-// Your Name
-// Date
-//
+// gridbased project
+// Rainn Morphy
+// Oct 26, 2024
+
+// For this assignment, you will build a game/simulation that includes 2D Arrays.
+ 
+// You can make whatever you want -- in the end, so long as you use a two-dimensional array in your project,
+// you are meeting the major requirement of this assignment. 
+// You are welcome to use the Game of Life demo as a starting point for this project. 
+// See the link below for some examples of what students have created in past semesters 
+// (a wide variety of quality of projects are represented). 
+
+// https://schellenberg.github.io/cs30-exemplar-projects/grid-based-game-exemplars/index.html
+
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
 
 function setup() {
