c6023fb6362685682fb2bc8f41a0af4a6af9c5c1
rainner <rainner_hmm@outlook.com>
2024-10-30 14:05:07 -0600

    i do not know what to do. :thumbsup:
    yikers

--- p03-gridbased/sketch.js
+++ p03-gridbased/sketch.js
@@ -6,7 +6,7 @@
 // Nov 8, 2024
 
 // For this assignment, you will build a game/simulation that includes 2D Arrays.
- 
+
 // You can make whatever you want -- in the end, so long as you use a two-dimensional array in your project,
 // you are meeting the major requirement of this assignment. 
 // You are welcome to use the Game of Life demo as a starting point for this project. 
@@ -17,9 +17,13 @@
 
 // Extra for Experts:
 
+let state = "start"; // sets the 'start' screen state
+
 let player = {
   x: 0,
   y: 0,
+  size: 50,
+  speed: 5,
 };
 
 function setup() {
@@ -28,4 +32,59 @@ function setup() {
 
 function draw() {
   background(220);
+  if (state === "start") {
+    titleState(); // displays 'start' screen
+  }
+  else if (state === "active") {
+    playerGraphics();
+    playerMovement();
+  }
 }
+
+function titleState() {
+  textAlign(CENTER, CENTER);
+  textSize(30);
+  text("press any button", width / 2, height / 2); // displays 'start' screen text
+}
+
+function mousePressed() {
+  if (state === "start") {
+    state = "active"; // switches 'start screen' state to an 'active' state, going right
+  }
+}
+
+function keyPressed() {
+  if (state === "start") {
+    state = "active";
+  }
+}
+
+/**
+ * wowza
+ */
+function playerMovement() {
+  if (state === "active") { // will give 4-directional movement to the user
+    if (keyIsPressed === true) {
+      if (key === "w") {
+        player.y -= player.speed;
+      }
+
+      if (key === "s") {
+        player.y += player.speed;
+      }
+
+      if (key === "a") {
+        player.x -= player.speed;
+      }
+
+      if (key === "d") {
+        player.x += player.speed;
+      }
+    }
+  }
+}
+
+function playerGraphics() {
+  square(player.x, player.y, player.size);
+}
+
